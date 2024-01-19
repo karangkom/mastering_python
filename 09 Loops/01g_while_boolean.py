a6c7b3ea111d68194fbe6749d93faf24dc80909a
Saddam Husein <saddambinhusein2@gmail.com>
2024-01-19 18:20:27 +0800

    Revising 01g_while_boolean.py

--- 09 Loops/01g_while_boolean.py
+++ 09 Loops/01g_while_boolean.py
@@ -13,7 +13,8 @@ while var1:
 """
 Sample 1 notes:
 - This sample is how to stop the infinite loop by changing the condition to False using if statement
-- count + 1 will make count start on 1
+- count + 1 will make count start on 1, but it never makes the count variable value change to 1
+- the count variable (e.g. count = 0) only changed by the increment statement (e.g. count += 1) 
 - with if condition the while loop will stop at 10 by changing the var1 to False
 """
 
@@ -26,7 +27,11 @@ while choice:
     a = int(input('Input value 1: '))
     b = int(input('Input value 2: '))
     print(f'{a} + {b} = {a + b}')
-    choice = input('Want to repeat (y/n)?: ')
-    if 'Y' != choice != 'y':
+    rep = input('Want to repeat (y/n)?: ')
+    if 'Y' != rep != 'y':
         choice = False
+
+    # or
+    # if rep == 'N' or rep == 'n':
+    #     choice = False
     print()
