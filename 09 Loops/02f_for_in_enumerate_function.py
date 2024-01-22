bf5169291ae8d330fa8e10719814c9c3e9c544dd
Saddam Husein <saddambinhusein2@gmail.com>
2024-01-22 13:32:11 +0800

    Revising 02f_for_in_enumerate_function.py

--- 09 Loops/02f_for_in_enumerate_function.py
+++ 09 Loops/02f_for_in_enumerate_function.py
@@ -4,3 +4,7 @@ idFoods1 = ['bakso', 'nasi padang', 'soto', 'coto makassar']
 
 for index, elm in enumerate(idFoods1): # index start on 0
     print(f'{index + 1}. {elm}')
+
+# same as:
+# for i in range(len(idFoods1)):
+#     print(f'{i + 1}. {idFoods1[i]}')
