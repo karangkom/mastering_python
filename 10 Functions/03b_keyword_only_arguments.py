6e10abb686f44b387b8d5fc148c5516a50d55bad
Saddam Husein <saddambinhusein2@gmail.com>
2024-01-23 13:07:35 +0800

    Revising 03b_keyword_only_arguments.py

--- 10 Functions/03b_keyword_only_arguments.py
+++ 10 Functions/03b_keyword_only_arguments.py
@@ -1,17 +1,17 @@
 """
 Keyword Arguments:
-Keyword arguments are passed by it’s name instead of their position as opposed to positional arguments in the function
-call. As a result we don’t have to mind about position of arguments when calling a function.
+Keyword arguments are passed by its name instead of their position in the function call. So as a result, 
+we don’t have to mind about the position of any argument that we have putted inside the argument parentheses.
 """
 def args(a, b):
     print(f'My name is {a}. I am {b} years old')
 
 
 args(a = 'Rahmat', b = 23)
-args(b = 31, a = 'saddam')  # Since we are passing arguments by their names/keywords order or position doesn’t matter
-args('fuad', b = 20)  # We can combine both positional and keyword arguments. but positional arguments should follows by keyword arguments
+args(b = 31, a = 'saddam')  # Since we are passing arguments through their names/keywords order, the arguments position doesn’t matter
+args('fuad', b = 20)  # We can combine both positional and keyword arguments. But positional arguments should be followed by keyword arguments
 
 # invalid keyword only arguments
 # args(50, 'mike')  # the position is incorrect
-# args('saddam')  # the count between arguments and parameters don’t match
-# args(b = 18, 'fitri')  # If we break the rule and pass arguments in such a way that keyword arguments follows by positional arguments which is a syntax error.
+# args('saddam')  # the number between argument and parameter are not same
+# args(b = 18, 'fitri')  # we should not pass the keyword argument first before the positional argument
